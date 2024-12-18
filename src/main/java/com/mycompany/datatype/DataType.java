61c270edce1923f170c80ec8ca4495629cc5e394
RaselDev28 <rasel.dev28@gmail.com>
2024-12-18 23:48:08 +0600

    first commit

---
+++ src/main/java/com/mycompany/datatype/DataType.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.datatype;
+
+public class DataType {
+
+    public static void main(String[] args) {
+        
+//        boolean x=true;
+        int p=20;
+//        char c="av";
+        
+        System.out.println("int is ="+p);
+    }
+}
